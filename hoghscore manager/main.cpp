ce955746be0f103033231c916ffba43021b6b9d5
moetime <morrice_pfeiffer@hotmail.com>
2016-07-30 14:40:48 -0400

    Sub menu created.

--- hoghscore manager/main.cpp
+++ hoghscore manager/main.cpp
@@ -1,93 +1,187 @@
+
 #include <iostream>
-#include <string>
-#include <sstream>
-
-// custom headers
-#include "user.h"
-#include "FileIO.h"
-
 using namespace std;
 
-void menuSelector(int); // prototype  
-void showMenu();
-int main() {
-
-	string fname;
-	string lname;
-	string userName;
-
-	int age;
-	int userInput;
-	int Score;
-	int highScore;
-	int lowScore;
+int ShowMenu();
+void ControlSubMenu();
+int ShowSubMenu();
 
 
-	//confirm a login by the user
-	cout << "Please login" << endl;
+int main()
+{
+	int choice = 1;
 
-	cin >> userName;
+	// end if user picks 4 or with invalid entry, continue program otherwise
+	while (choice != 5)
+	{
+		do {
+			if (choice > 5 || choice <= 0)
+			{
+				cout << "\We don't have that option. Please select again. \n";
+				cout << "\nPress <Enter> key to continue ... ";
+				fflush(stdin);
+				cin.get();
+				cin.get();
+			}
 
-		login(userName);
-		//login(userName);
+			choice = ShowMenu();
 
-
-
-
-	/*cout << "To start things we need some info from you." << endl;
-	cout << "\nPlease enter your first name:";
-	cin >> fname;
-	cout << "\nNow lets have your last name.";
-	cin >> lname;
-	cout << "\nYour age please?";
-	cin >> age;
-	cout << "\nPlease create a user name:";
-	cin >> userName;
-	cout << "\nSo your age is " << age << ".";
-	cout << "\nYour user name is " << userName << ".";
-	cout << "\n";*/
+		} while (choice < 0 || choice > 5);
 
 
+		if (choice != 5)
+			switch (choice)  // branch to an appropriate selection
+			{
+			case 1:
+				cout << "\nOption #1 was selected. Set the high score. \n";
+				cout << "\nPress <Enter> key to continue ... ";
+				//fflush(stdin);
+				cin.get();
+				cin.get();
+				break;
+			case 2:
+				cout << "\nOption #2 was selected. Show the high score. \n";
+				cout << "\nPress <Enter> key to continue ... ";
+				fflush(stdin);
+				cin.get();
+				cin.get();
+				break;
 
-	showMenu();
-	cin >> userInput;
-	menuSelector(userInput);
+			case 3:
+				cout << "\nOption #3 was selected. Update user.\n";
+				cout << "\nPress <Enter> key to continue ... ";
+				fflush(stdin);
+				cin.get();
+				cin.get();
+				ControlSubMenu();
+				break;
+			case 4:
+				cout << "\nOption #4 was selected. Delete user. \n";
+				cout << "\nPress <Enter> key to continue ... ";
+				fflush(stdin);
+				cin.get();
+				cin.get();
+				break;
 
+			}
+		else //choice 9 was selected
+		{
+			cout << "\nprogram is over. Thanks for using.\n";
+			cout << "\nPress <Enter> key to end the program. ";
+		}
+
+
+	} // end of while 
+	fflush(stdin);
 	cin.get();
-	cin.get();
-
 	return 0;
-}
+} // end of main
 
-
-
-void showMenu() {
-
-	cout << "\nPlease select a number 1-5:" << endl << endl;
-	cout << "\n1 Set Score:" << endl;
-	cout << "\n2 Show High Score:" << endl;
-	cout << "\n3 Update User:" << endl;
-	cout << "\n4 Delete User:" << endl;
-	cout << "\n5 To Exit:" << endl;
-}
-
-void menuSelector(int userInput) {
-	cout << "Input is " << userInput << endl;
-	switch (userInput) {
-	case 1:
-		cout << "Print high score:";
-		break;
-	case 2:
-		cout << "Show high score list:";
-		break;
-	case 3:
-		cout << "Update user information:";
-		break;
-	case 4:
-		cout << "Delete User:";
-		break;
-	case 5:
-		cout << "Press 5 to exit:";
-		break;
+int ShowMenu()
+{
+	int choice;
+	system("cls");
+	cout << "  	M E N U\n";
+	cout << "  	= = = = = = = =\n";
+	cout << "  1  Set score:\n";
+	cout << "  2  Show High Score:\n";
+	cout << "  3  Update User:\n";
+	cout << "  4  Delete User:.\n";
+	cout << "  5  End program: \n";
+	cout << "\nEnter your choice: ";
+	while (!(cin >> choice))
+	{
+		cout << "\nAre you drunk? that's invalid option."
+			<< "\nRe-Enter your choice: ";
+		cin.get();
+		cin.get();
+		cin.clear();
+		fflush(stdin);
 	}
+	return choice;
+
+} // end function ShowMenu
+
+
+void ControlSubMenu() {
+	int choice = 1;
+
+	// end if user picks 4 or with invalid entry, continue program otherwise
+	while (choice != 4)
+	{
+		do {
+			if (choice > 4 || choice <= 0)
+			{
+				cout << "\We don't have that option. Please select again. \n";
+				cout << "\nPress <Enter> key to continue ... ";
+				fflush(stdin);
+				cin.get();
+				cin.get();
+			}
+
+			choice = ShowSubMenu();
+
+		} while (choice < 0 || choice > 4);
+
+
+		if (choice != 4)
+			switch (choice)  // branch to an appropriate selection
+			{
+			case 1:
+				cout << "\nOption #1 was selected. distance english to metric. \n";
+				cout << "\nPress <Enter> key to continue ... ";
+				//fflush(stdin);
+				cin.get();
+				cin.get();
+				break;
+			case 2:
+				cout << "\nOption #1 was selected. distance metric to english. \n";
+				cout << "\nPress <Enter> key to continue ... ";
+				fflush(stdin);
+				cin.get();
+				cin.get();
+				break;
+
+			case 3:
+				cout << "\nOption #2 was selected. weight english to metric. \n";
+				cout << "\nPress <Enter> key to continue ... ";
+				fflush(stdin);
+				cin.get();
+				cin.get();
+				break;
+
+			}
+		else //choice 4 was selected
+		{
+			cout << "\nSubMenu is over. Thanks for using.\n";
+		}
+
+
+	} // end of while 
+
+
+
 }
+
+int ShowSubMenu() {
+	int choice;
+	system("cls");
+	cout << "  	User Profile\n";
+	cout << " = =  = = = = = = =\n";
+	cout << "  1  Edit first name.\n";
+	cout << "  2  Edit last name.\n";
+	cout << "  3  Edit age.\n";
+
+
+	cout << "  4  Back to main menu.\n";
+	cout << "\nEnter your choice: ";
+	while (!(cin >> choice))
+	{
+		cout << "\nAre you drunk? that's invalid option."
+			<< "\nRe-Enter your choice: ";
+		cin.clear();
+		fflush(stdin);
+	}
+	return choice;
+}
+
