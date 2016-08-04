777b46270ac22163443a35e944dc4dc0a585f394
adamrydall <adamrydall@hotmail.com>
2016-08-03 21:42:57 -0400

    added changes to fix merge

--- hoghscore manager/user.cpp
+++ hoghscore manager/user.cpp
@@ -9,79 +9,63 @@
 
 using namespace std;
 
-// Class
-class User
-{
-	//Structure for user information
-	private:
-		struct userInfo {
-			string	username,
-					firstName,
-					lastName;
-			int		age;
-		};
+//CONSTRUCTORS
+User::User()
+{ }
 
-		userInfo currentUser;
-			
-	//set the age, first & last names for the user
-	public:
-		//set
-		void setUserName(string newUserName) {
-			currentUser.username = newUserName;
-		}
-		void setFirstName(string newFirstName) {
-			currentUser.firstName = newFirstName;	//firstName
-		};
+User::User(string username, string firstName, string lastName, int age) {
 
-		void setLastName(string newLastName) {
-			currentUser.lastName = newLastName;		//lastName
-		};
+	setUserName(username);
+	setFirstName(firstName);
+	setLastName(lastName);
+	setAge(age);
+}
 
-		void setAge(int newAge) {		//age
-			currentUser.age = newAge;
-		};
+//DECONSTRUCTOR
+User::~User()
+{ }
 
-		//void setHighscore(int newHighScore) {
-		//	currentUser.highscore = newHighScore;	//highscore
-		//};
-
-		//get
-		string getFullName() {			//fullname
-			return currentUser.firstName + " " + currentUser.lastName;
-		}
-
-		int getAge() {					//age
-			return currentUser.age;
-		};
-
-		//int getHighScore() {			//highscore
-		//	return highscore;
-		//}
-		string getUserName() {
-			return currentUser.username;
-		}
-
-		//constructor
-		//User() {};
-};		
-	
-
-//METHODS
 //login a unique user
-void login(string newLogin) {
-	
+bool login(string newLogin) {
+
 	//check if the userName is registered
 	if (!checkUserExist(newLogin)) {
 
 		cout << "You've failed to login!" << endl;
 
-		//return false;
+		return false;
 	}
 	else {
 
 		cout << "You've successfully logged in!" << endl;
-		//return true;
+		return true;
 	}
+}
+
+	
+
+//METHODS
+//login a unique user
+bool login(string newLogin) {
+
+	//check if the userName is registered
+	if (!checkUserExist(newLogin)) {
+
+		cout << "You've failed to login!" << endl;
+
+		return false;
+	}
+	else {
+
+		cout << "You've successfully logged in!" << endl;
+		return true;
+	}
+}
+
+//check if the user exists
+bool checkUserExist(string username) {
+
+	return false;
 }
 
 //check if the user exists
@@ -117,33 +101,12 @@ bool checkUserExists(string username) {
 //update the user list
 void updateUserList() {
 	
-	//confirm the user is logged in
-	//if () {
-
-
-
-
-	//}
-	//else {
-		//log the user in
-		//login();
-	//}
 
 }
 
 //option for user to delete info
 void deleteUser() {
 
-	//confirm the user is logged in
-	//if () {
-
-
-
-
-	//}
-	//else {
-		//log the user in
-		//login;
-	//}
+	
 }
 
