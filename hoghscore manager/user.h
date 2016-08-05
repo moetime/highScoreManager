bccad5e44819015b3ff017279bfc2c86953ab887
macravenduarte <duarte.marco242@gmail.com>
2016-08-04 20:15:53 -0400

    ss

--- hoghscore manager/user.h
+++ hoghscore manager/user.h
@@ -2,52 +2,6 @@
 #include <string>
 using namespace std;
 
-// Class
-class User
-{
-private:
-	string	username;
-	string	firstName;
-	string	lastName;
-	int		age;
-
-	//set the age, first & last names for the user
-	public:
-		//set
-		void setUserName(string newUserName) {
-			username = newUserName;		//username
-		}
-		void setFirstName(string newFirstName) {
-			firstName = newFirstName;	//firstName
-		};
-
-		void setLastName(string newLastName) {
-			lastName = newLastName;		//lastName
-		};
-
-		void setAge(int newAge) {					//age
-			age = newAge;
-		};
-
-		//get
-		string getFullName() {						//fullname
-			return firstName +
-				" " + lastName;
-		}
-
-		int getAge() {								//age
-			return age;
-		};
-
-		string getUserName() {
-			return username;
-		}
-
-		User();
-		User(string, string, string, int);
-		~User();
-			
-};
 
 //class method prototypes
 bool login(string);
